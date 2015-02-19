9edbc34cbbeebdbf95cda8211d915adf9408cc31
Bratislav Petrovic <bratislav.petrovic@gmail.com>
2015-02-19 09:30:17 +0100

    first commit

---
+++ elpa/zen-and-art-theme-1.0.1/zen-and-art-theme-autoloads.el
@@ -0,0 +1,26 @@
+;;; zen-and-art-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "zen-and-art-theme" "zen-and-art-theme.el"
+;;;;;;  (21345 58200 64844 979000))
+;;; Generated autoloads from zen-and-art-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("zen-and-art-theme-pkg.el") (21345 58200
+;;;;;;  92121 334000))
+
+;;;***
+
+(provide 'zen-and-art-theme-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; zen-and-art-theme-autoloads.el ends here
---
+++ init.el
@@ -0,0 +1,39 @@
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(menu-bar-mode  -1)                       ;; turn-off the menu
+(tool-bar-mode -1)                       ;; turn-off toolbar 
+;; find file
+(defun find-file-at-point-with-line()
+  "if file has an attached line num goto that line, ie boom.rb:12"
+  (interactive)
+  (setq line-num 0)
+  (save-excursion
+    (search-forward-regexp "[^ ]:" (point-max) t)
+    (if (looking-at "[0-9]+")
+         (setq line-num (string-to-number (buffer-substring (match-beginning 0) (match-end 0))))))
+  (find-file-at-point)
+  (if (not (equal line-num 0))
+      (goto-line line-num)))
+
+;; ------------
+;; -- Macros --
+;; ------------
+(global-set-key "\C-h" 'backward-delete-char)
+(global-set-key "\C-x\C-i" 'find-file-at-point-with-line)
+
+(add-to-list 'load-path
+              "~/.emacs.d/yasnippet")
+(require 'yasnippet)
+(yas-global-mode 1)
+(autoload 'typing-of-emacs "typing" "The Typing Of Emacs, a game." t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("5e1d1564b6a2435a2054aa345e81c89539a72c4cad8536cfe02583e0b7d5e2fa" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
